51815ae10191c5f04a40a037b07b2599bddf1f8e
Rihan9 <33684020+Rihan9@users.noreply.github.com>
2021-01-27 14:41:27 +0100

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,4 +3,4 @@ import shutil, os
 # Rename root repo directory to use `-` instead of `_`.
 repo_dir = "{{cookiecutter.domain}}"
 new_repo_dir = repo_dir.replace("_", "-")
-shutil.copytree(os.path.join("..", repo_dir), os.path.join("..", new_repo_dir))
+shutil.copytree(os.path.join("..", repo_dir), os.path.join(".."))
